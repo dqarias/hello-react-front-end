aa2df83eb5ea0d81b092867b23eeeb4f748634b7
dqarias <dino.quispea@gmail.com>
2022-10-18 23:44:33 -0500

    Update the url for fetching data from backend

--- src/Services/greetingAPI.js
+++ src/Services/greetingAPI.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const fetchGreeting = createAsyncThunk('Greeting/fetchGreeting', async () => {
   const response = await axios
-    .get('http://127.0.0.1:3000/api/v1/greetings')
+    .get('http://localhost:3000/api/v1/greetings')
     .then((response) => response.data.message);
   return response;
 });
